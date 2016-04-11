80c7857ad43930a49ccbefa482932240a85e333e
Taka Okunishi <okunishitaka.com@gmail.com>
2016-04-11 15:06:39 +0900

    initial commit

---
+++ example/example-usage.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const apeDoc = require('ape-doc');
+
+// Javascript file path to read.
+let FILE_PATH = require.resolve('ape-doc/lib/comments_in_file');
+let comments = apeDoc(
+    FILE_PATH
+);
+
+assert.deepEqual(comments, [
+    {
+        "line": 0,
+        "description": "Extract comments from a file.",
+        "function": [
+            {
+                "name": "commentsInFile",
+                "optional": false,
+                "type": "",
+                "description": "",
+                "line": 2,
+                "source": "@function commentsInFile"
+            }
+        ],
+        "param": [
+            {
+                "type": "string",
+                "name": "filename",
+                "optional": false,
+                "description": "- Filename to read.",
+                "line": 3,
+                "source": "@param {string} filename - Filename to read."
+            }
+        ],
+        "returns": [
+            {
+                "type": "Object[]",
+                "name": "",
+                "optional": false,
+                "description": "",
+                "line": 4,
+                "source": "@returns {Object[]}"
+            }
+        ]
+    },
+    {
+        "line": 11,
+        "description": "",
+        "lends": [
+            {
+                "name": "commentsInFile",
+                "optional": false,
+                "type": "",
+                "description": "",
+                "line": 11,
+                "source": "@lends commentsInFile"
+            }
+        ]
+    }
+]);
---
+++ test/comments_in_file_test.js
@@ -0,0 +1,29 @@
+/**
+ * Test case for commentsInFile.
+ * Runs with mocha.
+ */
+"use strict";
+
+const commentsInFile = require('../lib/comments_in_file.js'),
+    assert = require('assert');
+
+describe('comments-in-file', () => {
+
+    before((done) => {
+        done();
+    });
+
+    after((done) => {
+        done();
+    });
+
+
+    it('Comments in file', (done) => {
+        let comments = commentsInFile(
+            require.resolve('../lib/comments_in_file')
+        );
+        console.log(JSON.stringify(comments, null, 2));
+        done();
+    });
+});
+
